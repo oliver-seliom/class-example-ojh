77d700a75d39c46676377023f992601d6b2f179d
Oliver Hoffman <ojh22@cornell.edu>
2024-09-23 16:54:09 +0200

    deleted all functions

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
 def sum_a_b(a, b):
   return a + b
-
-def new_function(a, b):
-  # dummy function
-  return a
-
-def subtract_a_b(a, b):
-  return a - b
-
-def multiply_a_b(a, b):
-  return b * a
-
-def dummy_function_2(a, b):
-  return b
