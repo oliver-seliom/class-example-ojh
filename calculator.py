399e539a4e6fab28ea43d8e973339453d6152d95
Willygoofy <garlogui@gmail.com>
2024-09-26 16:18:52 +0200

    funcionalities added

--- calculator.py
+++ calculator.py
@@ -1,13 +1,17 @@
 def add(x, y):
+    return x+y
     pass
 
 def subtract(x, y):
+    return x-y
     pass
 
 def multiply(x, y):
+    return x*y
     pass
 
 def divide(x, y):
+    return x/y
     pass
 
 def main():
