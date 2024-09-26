b70bffadb337d7fc80cde39e7d7ea46232ab91a4
oliver-seliom <95642873+oliver-seliom@users.noreply.github.com>
2024-09-26 16:14:59 +0200

    Revert "Nacho"

--- calculator.py
+++ calculator.py
@@ -1,14 +1,14 @@
 def add(x, y):
-    return x+y
+    pass
 
 def subtract(x, y):
-    return x - y
+    pass
 
 def multiply(x, y):
-    return x*y
+    pass
 
 def divide(x, y):
-    return x/y
+    pass
 
 def main():
     print("Welcome to the Calculator")
